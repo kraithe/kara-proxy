cacf94832c9c816d0e2796ab0edbdaea53bae190
Kara Raithe <55517663+kraithe@users.noreply.github.com>
2021-02-04 11:51:24 -0500

    Update local route

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const app = express();
 
 const proxy = require('express-http-proxy');
 const path = require('path');
 
 const proxyPort = 3876;
 const charityPort = 3987;
+const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/charity', proxy(`localhost:${charityPort}`));
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../public/index.html'))
+})
 
 app.listen(proxyPort, () => {
   console.log(`Charity proxy server listening on ${proxyPort}`);
 });
-
 
 module.exports = app;
